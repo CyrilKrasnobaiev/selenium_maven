f5190919a2d0fd6a3276f73d0dfc2ac8a379cd68
CyrilKrasnobaiev <kirill.krasnobaiev@gmail.com>
2017-04-12 15:45:04 +0300

    Maven tests with TestNG

--- src/test/java/EmailValidationData.java
+++ src/test/java/EmailValidationData.java
@@ -3,13 +3,13 @@ import org.testng.annotations.DataProvider;
 /**
  * Created by kirif on 11.04.2017.
  */
-public class EmailValidationData extends IncorrectEmailValidationTest{
-    @DataProvider(name = "validation")
+public class EmailValidationData{
+    @DataProvider
     public Object[][] incorrectEmailsData() {
         return new Object[][]{
                 {"2@e.e"},
-                {"erwerwer"},
-                {"434534534@f"}
+//                {"erwerwer"},
+//                {"434534534@f"}
         };
     }
 }
--- src/test/java/IncorrectEmailValidationTest.java
+++ src/test/java/IncorrectEmailValidationTest.java
@@ -1,41 +1,55 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
+
+
 
 /**
  * Created by kirif on 11.04.2017.
  */
 public class IncorrectEmailValidationTest extends BasicTest {
+
     @BeforeClass
     void beforeClass() {
-        System.setProperty("webdriver.chrome.driver", "C:/work/chromedriver_win32/chromedriver.exe");
-            driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "C:/work/chromedriver_win32/chromedriver.exe");
+        driver = new ChromeDriver();
         }
 
     @Test
-    public void getNeededPage() {
+    public void neededPageTest() {
         driver.get("https://templatemonster.com");
         driver.findElement(By.id("header-signin-link")).click();
-        final Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
         ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs2.get(1));
+        final Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("id-index-continue-button"))));
     }
 
 
-    @Test(dataProvider = "validation")
-    public void EmailValidation(String nonValidate1, String nonValidate2, String nonValidate3){
-        System.out.println(nonValidate1 + nonValidate2 + nonValidate3);
+    @Test(dependsOnMethods = "neededPageTest", dataProvider = "incorrectEmailsData", dataProviderClass = EmailValidationData.class)
+    public void emailValidationTest(String invalidEmail) {
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys(invalidEmail);
+        driver.findElement(By.id("id-index-continue-button")).click();
+        final Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[@class='notification']"))));
+        String errorTest = driver.findElement(By.xpath("//span[@class='notification']")).getText();
+        System.out.println(errorTest);
+//        if(errorTest == "Please specify a valid email") {
+//            System.out.println("Fuck Yeahh!!!");
+//        }
+//        else{
+//            System.out.println("Fuck No!!!");
+//        }
     }
 }
+
 
 
 
